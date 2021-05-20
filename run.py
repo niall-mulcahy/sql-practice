c5ac897e1a4fb9ca69c2888b7f00032cd3bb94dd
niall-mulcahy <niall-mulcahy@outlook.com>
2021-05-20 13:51:02 +0000

    added display functionality which allows me to pass data to html using jinja

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 import pymysql
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 from datetime import datetime
 
 
@@ -21,13 +21,25 @@ def getvalue():
     Lname = request.form['LastName']
     mail = request.form['Email']
     row = (Fname, Lname, mail)
-    connection = pymysql.connect(host='localhost', user='root', passwd='', db='Chinook')
+    connection = pymysql.connect(
+        host='localhost', user='root', passwd='', db='Chinook')
     with connection.cursor() as cursor:
         cursor.execute("INSERT INTO Customer (FirstName, LastName, Email) Values (%s, %s, %s);", row)
         connection.commit()
         connection.close()
     return render_template("index.html")
 
+
+@app.route("/display")
+def display():
+    connection = pymysql.connect(host='localhost', user='root', passwd='', db='Chinook')
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT CustomerID, FirstName, LastName, Email from Customer where CustomerID > 65")
+        connection.commit()
+        result = cursor.fetchall()
+        connection.close()
+    return render_template(
+        "display.html", result=result)
 
 
 if __name__ == "__main__":
